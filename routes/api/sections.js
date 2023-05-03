a6d84a285123a5cee7f1b4df07e17914e2999822
Roman <roman.ustychenko@gmail.com>
2023-05-03 08:51:14 +0300

    Update sections.js

--- routes/api/sections.js
+++ routes/api/sections.js
@@ -5,17 +5,18 @@ const { ctrlWrapper } = require("../../helpers");
 const { validate, isValidId, authenticate } = require("../../middlewares");
 const {schemas} = require("../../models/sections");
 
-// router.get("/", authenticate, ctrlWrapper(ctrl.listSection));
-
-// router.get("/:id", authenticate, isValidId, ctrlWrapper(ctrl.getSectionById));
-
 router.get("/:owner",  ctrlWrapper(ctrl.getSectionsByName));
+router.get("/", authenticate, ctrlWrapper(ctrl.listSection));
 
-// router.post("/", authenticate, validate(schemas.addSectionSchema), ctrlWrapper(ctrl.addSection));
-
-// router.put("/:id", authenticate, isValidId, validate(schemas.addSectionSchema), ctrlWrapper(ctrl.updateSection));
+router.get("/:id", authenticate, isValidId, ctrlWrapper(ctrl.getSectionById));
 
 
-// router.delete("/:id", authenticate, isValidId, ctrlWrapper(ctrl.removeSection));
+
+router.post("/", authenticate, validate(schemas.addSectionSchema), ctrlWrapper(ctrl.addSection));
+
+router.put("/:id", authenticate, isValidId, validate(schemas.addSectionSchema), ctrlWrapper(ctrl.updateSection));
+
+
+router.delete("/:id", authenticate, isValidId, ctrlWrapper(ctrl.removeSection));
 
 module.exports = router;
