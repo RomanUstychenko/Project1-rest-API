865dbedda51ff6e444a4739345bd6cfc7e04bae4
Roman <roman.ustychenko@gmail.com>
2023-12-16 17:48:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ mongoose.set('strictQuery', true);
 
 const app = require("./app");
 
-const {DB_HOST, PORT = 3000} = process.env;
+const {DB_HOST, PORT = 3001} = process.env;
 
 mongoose.connect(DB_HOST, console.log("Database connection successful"))
 .then(()=> app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000");
+  console.log(`Server running. Use our API on port: ${PORT}`);
 }))
 .catch(error => {
   console.log(error.message);
