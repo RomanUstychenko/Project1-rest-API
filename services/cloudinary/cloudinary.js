c2a84d34413d936f2e65ae356451529127cc71ca
Roman <roman.ustychenko@gmail.com>
2024-08-21 01:16:34 +0300

    added group delete

--- services/cloudinary/cloudinary.js
+++ services/cloudinary/cloudinary.js
@@ -63,5 +63,36 @@ exports.deleteImage = async (req, res) => {
     console.warn('Unexpected result:', result);
     return res.status(500).json({ error: 'Unexpected result', result });
   });
-
   };
+
+  exports.deleteImagesSection = async (req, res) => {
+    let publicIds = req.body.public_ids; // Масив public_id зображень для видалення
+
+    // Переконайтеся, що отримуєте масив і обробляєте його коректно
+    console.log('Received public_ids:', publicIds);
+  
+   
+    // Логування оброблених publicIds
+    console.log('Processed public_ids:', publicIds);
+  
+    cloudinary.api.delete_resources(publicIds, (error, result) => {
+      if (error) {
+        console.error('Error deleting images:', error); // Вивід помилки в консоль
+        return res.status(500).json({ error: 'Failed to delete images' });
+      }
+      
+      // Логування результату
+      console.log('Cloudinary delete result:', result);
+  
+      // Перевірка, чи всі зображення успішно видалені
+      const deletedImages = result.deleted;
+      const notFoundImages = Object.keys(deletedImages).filter(id => deletedImages[id] === 'not_found');
+  
+      if (notFoundImages.length > 0) {
+        console.warn('Some images not found:', notFoundImages);
+        return res.status(404).json({ message: 'Some images not found', notFoundImages });
+      }
+  
+      res.status(200).json({ message: 'All images deleted successfully', result });
+    });
+  }
