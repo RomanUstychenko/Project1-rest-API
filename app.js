d581ab2a24a8065d46862637bc62d57235213230
Roman <roman.ustychenko@gmail.com>
2024-09-19 15:22:17 +0300

    Dockerfile

--- app.js
+++ app.js
@@ -16,42 +16,6 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 
-////////////////
-
-const dotenv = require('dotenv');
-const cloudinary = require('cloudinary').v2;
-const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
-
-dotenv.config();
-
-
-// Configuration 
-cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET
-});
-
-const router = express.Router();
-
-app.use(express.json());
-
-router.delete('/deleteImage', (req, res) => {
-  const publicId = req.body.public_id; // public_id зображення, яке треба видалити
-  console.log("publicId", publicId)
-  cloudinary.uploader.destroy(publicId, (error, result) => {
-    if (error) {
-      return res.status(500).json({ error: 'Failed to delete image' });
-    }
-    return res.status(200).json({ message: 'Image deleted successfully' });
-  });
-});
-
-app.use('/project1', router)
-
-///////////////
-
-
 // send Email nodemailer /////
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -75,12 +39,6 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message: err.message });
 });
-
-
-
-
-
-
 
 
 module.exports = app;
--- controllers/items/getItemBySection.js
+++ controllers/items/getItemBySection.js
@@ -5,28 +5,20 @@ const getItemBySection = async (req, res) => {
   const { _id: owner } = req.user;
   const { category } = req.params;
 
-  //   // Pagination--------------
-  const { page = 1, limit = 8 } = req.query;
-  const skip = (page - 1) * limit;
-  //   const total = await Notice.count(buildQuery(req, { adopStatus: category }));
-  //   const totalPages = Math.ceil(total / limit);
-  //   // ------------------------
-
   const result = await Item.find(
     buildQuery(req, {
       section: category,
       owner: owner,
     })
   ).populate("owner", "email");
-  // const result = await Item.find({owner}, "", {skip, limit}).populate("owner", "email")
-  // .populate("owner", "email phone")
-  // .sort({ createdAt: -1 })
 
-  // .limit(limit)
-  // .skip(skip);
+  if (result.length === 0) {
+    throw HttpError(404, `No items in "${category}" section`);
+  }
+
   result.sort((a, b) => {
-    const idSortA = parseInt(a.idSort);
-    const idSortB = parseInt(b.idSort);
+    const idSortA = parseInt(a.idSort, 10);
+    const idSortB = parseInt(b.idSort, 10);
 
     if (idSortA < idSortB) return -1;
     if (idSortA > idSortB) return 1;
@@ -34,9 +26,7 @@ const getItemBySection = async (req, res) => {
     return 0;
   });
 
-  if (result.length === 0) {
-    throw HttpError(404, `No items in "${category}" section`);
-  }
+ 
 
   res.json(result);
 };
